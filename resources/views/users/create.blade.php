9ce29c3e0f411e6042245d91759a50718a7b90fc
PasaporteApp <proyectoprueba780@gmail.com>
2024-07-18 17:49:05 -0500

    Cambio de terminos y condiciones

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -90,7 +90,8 @@
                             <span id="password-error" class="error-message mensajeErrorContra"></span>
                             <div class="mb-3 form-check">
                                 <input type="checkbox" class="form-check-input" id="privacy_terms" required>
-                                <label class="form-check-label" for="privacy_terms">Acepto los términos de privacidad</label>
+                                <label class="form-check-label" for="privacy_terms">Acepto los <términos de privacidad><a href="http://https://drive.google.com/drive/u/3/my-drive"></a></privacidad></label>
+
                             </div>
                             <div class="mb-3 form-check">
                                 <input type="checkbox" class="form-check-input" id="is_company" name="is_company">
